df76ee4b5eeb6468ec4c1da5701b21cc2e1aa2e5
Emilio <franceschini.emilio@gmail.com>
2024-02-01 00:13:44 -0300

    clean home, logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Image from 'next/image';
 import {
-  //MdMenu,
+  MdMenu,
   MdHome,
   MdVideogameAsset,
   MdAccountCircle,
@@ -11,17 +11,6 @@ import { sen } from '@/app/fonts';
 export const runtime = 'edge';
 
 export default function Mobile() {
-  let currentScore = 80;
-  let maxScore = 100;
-  let scoreColor = () => {
-    if (currentScore / maxScore < 0.3) {
-      return 'text-danger-400';
-    } else if (currentScore / maxScore < 0.7) {
-      return 'text-warning-400';
-    } else {
-      return 'text-success-400';
-    }
-  };
   const navBorderColor = () => {
     return `
     bg-accent-500
@@ -31,76 +20,17 @@ animate-gradient-x`;
     <div className='flex flex-col h-[100dvh] w-[100dvw] bg-bg-400 antialiased'>
       <nav className='h-14 w-full'>
         <div className='px-4 flex w-full mx-auto h-full items-center justify-between'>
-          <img src='/logo.svg' className='w-[41px] h-[41px]' alt='logo' />
+          <img src='/logo.svg' className='w-[111px] h-[]' alt='logo' />
           <div className='flex justify-center'>
-            <span className='block text-2xl text-accent-300 '>
-              <span className={`${scoreColor()}`}>{currentScore}</span>/
-              {maxScore}
-            </span>
+            <MdMenu color='#4C6477' size='36px' />
           </div>
         </div>
       </nav>
       <main
         className={`flex flex-1 flex-col w-full ${navBorderColor()} rounded-xl py-[2px]`}>
         <div className='flex flex-1 flex-col w-full bg-bg-100 rounded-xl'>
-          <div className='flex flex-col w-full py-10 px-10  justify-center items-center'>
-            <div className='flex flex-col w-full h-full items-center justify-center rounded-xl border border-primary-600 border-b-2 border-b-accent-500 bg-bg-300 '></div>
-          </div>
-          <div className='flex flex-col w-full h-full px-10 pb-10 justify-center items-center gap-3'>
-            <div className='absolute left-10'>
-              <MdOutlineStar
-                className='animate-star-victory opacity-0'
-                color='#FFDD87'
-                size='18px'
-              />
-            </div>
-            <div className='absolute left-7'>
-              <MdOutlineStar
-                className='animate-star-victory opacity-0'
-                color='#FFDD87'
-                size='18px'
-              />
-            </div>
-            <div className='absolute left-0'>
-              <MdOutlineStar
-                className='animate-star-victory opacity-0'
-                color='#FFDD87'
-                size='18px'
-              />
-            </div>
-            <div className='absolute right-10'>
-              <MdOutlineStar
-                className='animate-star-victory opacity-0'
-                color='#FFDD87'
-                size='18px'
-              />
-            </div>
-            <div className='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
-              <span className={`${sen.className} text-2xl text-primary-300`}>
-                Charmander
-              </span>
-            </div>
-            <div className='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
-              <span className={`${sen.className} text-2xl text-primary-300`}>
-                Charmander
-              </span>
-            </div>
-            <div className='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
-              <span className={`${sen.className} text-2xl text-primary-300`}>
-                Charmander
-              </span>
-            </div>
-            <div className='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
-              <span className={`${sen.className} text-2xl text-primary-300`}>
-                Charmander
-              </span>
-            </div>
-            <div className='flex w-full flex-1 items-center justify-center rounded-md border-b border-primary-500 bg-primary-700 '>
-              <span className={`${sen.className} text-2xl text-primary-300`}>
-                Charizard
-              </span>
-            </div>
-          </div>
+          <div className='flex flex-col w-full py-10 px-10  justify-center items-center'></div>
+          <div className='flex flex-col w-full h-full px-10 pb-10 justify-center items-center gap-3'></div>
         </div>
       </main>
       <nav className='h-14 w-full'>
