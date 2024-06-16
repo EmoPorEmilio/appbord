fd1733dc6f28fdee0c66a0021ebbdfa4a3eb0f15
Emilio <franceschini.emilio@gmail.com>
2024-06-16 16:49:25 -0300

    onfocus distinto icon

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import {
   MdOutlineStar,
 } from 'react-icons/md';
 import { sen } from '@/app/fonts';
+import { useEffect, useMemo, useState } from 'react';
 export const runtime = 'edge';
 
 export default function Mobile() {
@@ -16,7 +17,39 @@ export default function Mobile() {
     bg-accent-500
 animate-gradient-x`;
   };
+  const [_, setIntv] = useState<any>(null);
+  const document = typeof window !== 'undefined' ? window.document : null;
+
+  useEffect(() => {
+    const new_intv = setInterval(()=>{
+      console.log("test");
+            var link = document?.querySelector("link[rel~='icon']");
+      if (!link) {
+          link = document!.createElement('link');
+          link.rel = 'icon';
+          document!.head.appendChild(link);
+      }
+      if (document?.hasFocus()) {
+      link.href = '/favicon-on.png' ;
+
+      } else {
+      link.href = '/favicon-off.png';
+
+      }
+    }, 50);
+    setIntv(new_intv);
+    return () => {
+      setIntv((prev_intv: any)=>{
+        clearInterval(prev_intv);
+        return null;
+      }
+      );
+    };
+  }, [document]);
+
   return (
+    <>
+    <link rel="icon" type="image/png" href="favicon-on.png" sizes="any" />
     <div className='flex flex-col h-[100dvh] w-[100dvw] bg-bg-400 antialiased'>
       <nav className='h-14 w-full'>
         <div className='px-4 flex w-full mx-auto h-full items-center justify-between'>
@@ -40,6 +73,6 @@ animate-gradient-x`;
           <MdAccountCircle color='#4C6477' size='36px' />
         </div>
       </nav>
-    </div>
+    </div></>
   );
 }
